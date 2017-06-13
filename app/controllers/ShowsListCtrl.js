9ded725bfc826045980a9e4dcff9f804e9f50800
Chaz Henricks <chazhenricks@gmail.com>
2017-06-13 11:03:26 -0500

    login/filter results and link with directions included

--- app/controllers/ShowsListCtrl.js
+++ app/controllers/ShowsListCtrl.js
@@ -3,16 +3,42 @@
 app.controller("ShowsListCtrl", function($scope, AuthFactory, DataFactory, Spotify, $location, localStorageService){
 
 
-let getTopArtists = ()=>{
-    Spotify.getUserTopArtists({ limit: 50, time_range: "short_term" })
-    .then(function (data) {
-        $scope.artistArray = data.data.items;
-      console.log($scope.artistArray);
+
+let getArtistsShows = function(artists){
+    $scope.localShows = [];
+    artists.forEach((item)=>{
+    DataFactory.getShows(item)
+        .then((response)=>{
+            response.name = response.name.replace(/%20/g, ' ');
+
+            $scope.localShows.push(response);
+        },(error)=>{
+            console.log(error);
+        });
     });
+        console.log("localShows",$scope.localShows);
 };
 
 
+//Gets the last 50 top artists played within the last:
+    // short_term: few weeks
+    // medium_term: few months
+    // long_term: few years
 
+let getTopArtists = ()=>{
+    Spotify.getUserTopArtists({ limit: 50, time_range: "medium_term" })
+    .then(function (data) {
+        var artists = [];
+        var arrayFromSpotify = data.data.items;
+        arrayFromSpotify.forEach(function(item){
+            var artistData = {};
+            artistData.name = item.name;
+            artistData.picture = item.images[0].url;
+            artists.push(artistData);
+        });
+        getArtistsShows(artists);
+    });
+};
 
 getTopArtists();
 
--- app/factories/DataFactory.js
+++ app/factories/DataFactory.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.factory("DataFactory", function($q, $http, $window, FBCreds){
+app.factory("DataFactory", function($q, $http, $window, FBCreds, BITCreds){
 
 
 const addUser = function(newUser){
@@ -16,10 +16,37 @@ const addUser = function(newUser){
    });
 };
 
+const getShows = function(artist){
+    artist.name = artist.name.replace(/\s/g, '%20');
+    return $q((resolve, reject)=>{
+        $http.get(`${BITCreds.databaseURL}/artists/${artist.name}/events?app_id=shows_around`)
+        .then((response)=>{
+            var showsArray = response.data;
+            showsArray.forEach((show)=>{
+                if(show.venue.city === "Nashville"){
+                    artist.city = show.venue.city;
+                    artist.state = show.venue.region;
+                    artist.venue = show.venue.name;
+                    artist.tickets = show.offers[0].url;
+                    artist.lat = show.venue.latitude;
+                    artist.long = show.venue.longitude;
+
+                    resolve(artist);
+                }
+            });
+            // console.log("SHOWS RESPONSE", response);
+        })
+        .catch((error)=>{
+            reject(error);
+        });
+    });
+};
+
 
 
 return{
-    addUser
+    addUser,
+    getShows
     };
 
 });
