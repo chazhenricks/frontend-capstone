5e02450da6c925ffab8520bc098ee97d788a9d6f
Chaz Henricks <chazhenricks@gmail.com>
2017-06-21 11:43:06 -0500

    still working on figureing out redirect uri stuff

--- docs/app/values/spotify-creds.js
+++ docs/app/values/spotify-creds.js
@@ -4,6 +4,6 @@
 //spotify creds. needed to talk with spotify
 app.constant("SpotifyCreds", {
     ClientId:"56e27aa1be0549c8a604ea4e23594fd1",
-    RedirectUri: "https://chazhenricks.github.io/login.html",
+    RedirectUri: "https://chazhenricks.github.io/frontend-capstone/login.html",
     Scope: "user-library-read user-top-read"
 });
